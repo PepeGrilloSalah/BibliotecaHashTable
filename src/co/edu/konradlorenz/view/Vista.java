52ba5ca613189430fb768395765d502138ade7bf
PepeGrilloSalah <pipelgrisli@gmail.com>
2024-10-27 20:22:20 -0500

    Update Vista.java
    menu y vista 4k

--- src/co/edu/konradlorenz/view/Vista.java
+++ src/co/edu/konradlorenz/view/Vista.java
@@ -8,8 +8,19 @@ public class Vista {
     public static void mostrarMensaje(String mensaje){
         System.out.println(mensaje);
     }
-    
-    public static String pedirString(){
+    public static void mostrarMenu() {
+    System.out.println("------ Menú de Biblioteca ------");
+    System.out.println("1. Agregar libros a la biblioteca");
+    System.out.println("2. Prestar libro");
+    System.out.println("3. Devolver libro");
+    System.out.println("4. Mostrar los libros de la biblioteca");
+    System.out.println("5. Eliminar un libro de la biblioteca");
+    System.out.println("6. Salir");
+    System.out.print("Seleccione una opción: ");
+}
+
+    public static String pedirString(String dato){
+        System.out.println(dato);
     	Vista v = new Vista();
         return v.sc.nextLine();
     }
